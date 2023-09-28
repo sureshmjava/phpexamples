bbd2797bba8ac84312b4a2e0b3fe0c70f26864e3
suresh <sureshmjava83@gmail.com>
2023-09-28 23:55:34 +0530

    run the program

---
+++ hello.php
@@ -0,0 +1,11 @@
+<?php
+    $message = "Hello, World!";
+    $count = 30;
+    $float_value=1.56;
+
+    var_dump($message);
+    var_dump($count);
+    var_dump($float_value);
+
+    echo $message;
+    
