fed4c689af2cfe27cc398dcc6f5e29b234cdcac0
suresh <sureshmjava83@gmail.com>
2023-09-30 00:21:09 +0530

    define the array as associative array

--- array.php
+++ array.php
@@ -1,4 +1,20 @@
 <?php
- $array_var=["hai","Hello","Welcome"];
- #var_dump($array_var);
- var_dump($array_var[1]);
+ $array_var=["hai",
+            "Hello",
+            "third"=>"Welcome"];
+
+ echo "print the array:";
+ var_dump($array_var);
+
+ echo "print the array element through index:";
+ var_dump($array_var["third"]);
+
+ echo "print the Array by using foreach :\n";
+ foreach($array_var as $value){
+    echo $value.",";
+ }
+
+ echo "\n print with index:\n";
+ foreach($array_var as $index => $value){
+    echo $index . "-" .$value.",";
+ }
